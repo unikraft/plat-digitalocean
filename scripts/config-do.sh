17241572bd8b3fc1cd6e63e0a8c47fe71db94e6a
Mujahid Ali <mujahid.ali@india.nec.com>
2019-10-17 20:19:47 +0200

    Configuration file for core script

---
+++ scripts/config-do.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# "deploy-unikraft-do.sh" script needs all the environmrntal variables
+# mentioned below to be set correctly. Any unset value or incorrect value
+# will cause the script to halt.
+
+# DO Authorization Token
+export DO_AUTH_TOKEN=""
+# Fingerprint of your public ssh-key which you've added to digitalocean
+export DO_AUTH_FINGER_PRINT=""
+
